2e7bf720ec80a804e21c847b1a61c25dc7773a44
Bongani Masango <bkmasango26@gmail.com>
2023-04-18 20:03:23 +0200

    expo-location

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,76 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import { StyleSheet, Text, View, SafeAreaView, Alert } from 'react-native'
+import * as Location from 'expo-location'
 
 const HomeScreen = () => {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
+    const [displayCurrentAddress, setdisplayCurentAddress] = useState("We are loading your location");
+    const [locationServicesEnabled, setlocationServicesEnabled] = useState(false);
+
+
+    useEffect(() => {
+      checkIfLocationEnabled();
+      getCurrentLocation();
+    }, [])
+
+    const checkIfLocationEnabled = async () => {
+        let enabled = await Location.hasServicesEnabledAsync();
+        if(!enabled) {
+            Alert.alert('Location services not enabled', 'Please enable the location services', [
+                {
+                  text: 'Cancel',
+                  onPress: () => console.log('Cancel Pressed'),
+                  style: 'cancel',
+                },
+                {text: 'OK', onPress: () => console.log('OK Pressed')},
+              ]);
+        } else {
+            setlocationServicesEnabled(enabled);
+        }
+    }
+
+    const getCurrentLocation = async () => {
+        let {status} = await Location.requestForegroundPermissionsAsync();
+
+        if(status !== "granted") {
+            Alert.alert('Permission denied', 'allow the app to use the location services', [
+                {
+                  text: 'Cancel',
+                  onPress: () => console.log('Cancel Pressed'),
+                  style: 'cancel',
+                },
+                {text: 'OK', onPress: () => console.log('OK Pressed')},
+              ],
+              { cancelable: false }
+
+            );
+        }
+        
+        const {coords} = await Location.getCurrentPositionAsync();
+        console.log(coords);
+    if(coords) {
+        const {latitude, longitude} = coords
+
+        let response = await Location.reverseGeocodeAsync({
+            latitude,
+            longitude
+        })
+
+        console.log(response);
+
+        for(let item of response.results) {
+            let address = `${item.name} ${item.city} ${item.postalCode}`
+            setdisplayCurentAddress(address)
+        }
+    }
+    
 }
+
+  return (
+    <SafeAreaView>
+      <Text>HomeScreen</Text>
+    </SafeAreaView>
+  )
+  }
 
 export default HomeScreen
 
